3f5a432b1b3dc7a5caf128d30f7f47a4bd18980a
Your Name <you@example.com>
2025-05-02 20:20:51 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -101,43 +101,47 @@ void removeNode(TreeMap * tree, TreeNode* node) {
     // Si el nodo no existe no ocurre nada
     if( node == NULL) return;
 
-    // EL NODO NO TIENE HIJOS
+    // CASO 1: EL NODO NO TIENE HIJOS
     if (node->left == NULL && node->right == NULL){
-        // Si estamos en el nodo que es la RAIZ, su padre sera NULL, liberamos el nodo y dejamos la raiz en null
+        // Si el padre es NULL estamos en la raiz
         if(node->parent == NULL){
-            free(node->pair);
-            free(node);
             tree->root = NULL;
         }
         // Si no estamos en la raiz tenemos que identificar si es hijo izq. o der.
         // El nodo actual (node) es el hijo izquierdo de su padre (parent)?
         else if(node->parent->left == node){
-            TreeNode* child = (node->left != NULL) ? node->left : node->right;
-            node->parent->left = child;
-            free(node->pair);
-            free(node);
             node->parent->left = NULL;
         }
         // Aqui estamos cuando el nodo sea el hijo derecho
-        else{
-            TreeNode* child = (node->right != NULL) ? node->left : node->right;
-            node->parent->right = child;
-            free(node->pair);
-            free(node);
-            node->parent->right = NULL;
-        }
-    }
-    // EL NODO TIENE UN HIJO
-    else if((node->left == NULL && node->right != NULL) || (node->left != NULL && node->right == NULL)){
-        if(node->parent->left == node){
-            node->parent->left = NULL;
-        }
         else{
             node->parent->right = NULL;
         }
         free(node->pair);
         free(node);
     }
+    // CASO 2: EL NODO TIENE UN SOLO HIJO
+    else if((node->left == NULL && node->right != NULL) || (node->left != NULL && node->right == NULL)){
+        TreeNode* child = (node->left != NULL) ? node->left : node->right;
+
+        // Si estamos en la raiz
+        if(node->parent == node){
+            tree->root = child;
+            child->parent = NULL;
+            // la raiz del arbol ahora sera el hijo que encontramos
+            // y ahora a este child su padre lo dejamos en NULL, porque es la nueva raizzz
+        }
+        else if(node->parent->left == node){
+            node->parent->left = child;
+            child->parent = node->parent;
+        } else {
+            node->parent->right = child;
+            child->parent = node->parent;
+        }
+        free(node->pair);
+        free(node);
+    }
+
+    // CASO 3: NODO CON DOS HIJOS
 }
 
 void eraseTreeMap(TreeMap * tree, void* key){
